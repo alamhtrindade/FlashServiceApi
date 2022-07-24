4379f1e9c1d025a4e4d73f8f226234c5d1762150
alamhtrindade <alamhtrindade@gmail.com>
2022-07-24 17:48:57 -0400

    teste domingo a noite

--- index.php
+++ index.php
@@ -1,6 +1,35 @@
 <?php
 
-$data = ["Status:"," OK"];
-echo json_encode($data)
+$db = Database::singleton();
+
+$sql = 'SELECT * FROM ' . self::_table . ' ORDER BY id DESC';
+
+$sth = $db->prepare($sql);
+
+$sth->execute();
+
+echo '<pre>'
+
+if($obj = $sth->fetch(PDO::FETCH_OBJ)){
+  
+  $user = new User();
+
+  $user->setId($obj->id);
+  $user->setName($obj->name);
+  $user->setEmail($obj->email);
+  $user->setPhone($obj->phone);
+  $user->setAddress($obj->address);
+  $user->setPassword($obj->password);
+  
+  echo json_encode($user);
+
+}else{
+  echo('Nao executou busca');
+}
+
+
+echo('Terminado');
+
+
 
 ?>
