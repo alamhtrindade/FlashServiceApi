8b34816413a67c00e7c1249859ab7d4157c9367c
alamhtrindade <alamhtrindade@gmail.com>
2022-07-24 18:03:21 -0400

    dasda

--- index.php
+++ index.php
@@ -1,32 +1,6 @@
 <?php
 
-require_once('common-class/Database.php');
-require_once('config/autoload.php');
-
-$db = Database::singleton();
-
-$sql = 'SELECT * FROM _user ORDER BY id DESC';
-
-$sth = $db->prepare($sql);
-
-$sth->execute();
-
-if($obj = $sth->fetch(PDO::FETCH_OBJ)){
-  
-  $user = new User();
-
-  $user->setId($obj->id);
-  $user->setName($obj->name);
-  $user->setEmail($obj->email);
-  $user->setPhone($obj->phone);
-  $user->setAddress($obj->address);
-  $user->setPassword($obj->password);
-  
-  print_r($user);
-
-}else{
-  echo('Nao executou busca');
-}
+teste
 
 
 ?>
